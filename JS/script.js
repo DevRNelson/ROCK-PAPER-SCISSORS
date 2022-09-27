23a7b2fe48457caace6856ab41d754cefd3d452c
Rinaldo Nelson <rinaldo.nelson1@gmail.com>
2022-09-27 14:51:47 -0400

    added player and computer score calls

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 const playerScore = document.querySelector("#playerScore")
-const computerScore = document.querySelector("computerScore")
+const computerScore = document.querySelector("#computerScore")
 const choiceBtns = document.querySelectorAll(".choiceBtn")
 let rock = "ROCK!"
 let paper = "PAPER!"
@@ -8,10 +8,26 @@ let playerSelection;
 let computer;
 
 choiceBtns.forEach(button.addEventListener("click", () => {
+
     playerSelection = button.textContent;
-    computerTurn();
+    computerChoice();
+    playerScore.textContent = `Player: ${playerScore}`;
+    computerScore.textContent = `Player: ${computerScore}`;
+    resultText.textContent = checkWinner();
 }));
 
-function computerTurn() {
+function computerChoice() {
     const randNum = math.floor(math.random() * 3) + 1;
+
+    switch (randNum) {
+        case 1:
+            computer = "ROCK";
+            break;
+        case 2:
+            computer = "PAPER";
+            break
+        case 3:
+            computer = "SCISSORS";
+            break
+    }
 }
