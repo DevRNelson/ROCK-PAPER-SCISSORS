d71eec103a2169643d181927ff26410a8e811ea0
Rinaldo Nelson <rinaldo.nelson1@gmail.com>
2022-09-27 13:02:22 -0400

    css touches

--- JS/script.js
+++ JS/script.js
@@ -1,33 +1,33 @@
 let rock = "ROCK!"
 let paper = "PAPER!"
 let scissors = "SCISSORS"
-let playerSelection = parseInt(prompt("Please select your number!  |1 = Rock | 2 = Paper | 3 = Scissors|"));
+let playerSelection =
 
 
 
-function playRound() {
-    let computerChoice = math.floor(math.random() * 3)
+    function playRound() {
+        let computerChoice = math.floor(math.random() * 3)
 
-    if (playerSelection === getComputerChoice) {
-        console.log("it's a tie!")
-    } else if (computerChoice === 1) {
-        if (playerSelection === 2) {
-            console.log("Congragulations you beat Rock with Paper!!")
+        if (playerSelection === getComputerChoice) {
+            console.log("it's a tie!")
+        } else if (computerChoice === 1) {
+            if (playerSelection === 2) {
+                console.log("Congragulations you beat Rock with Paper!!")
+            } else {
+                console.log("Sorry you lost Rock")
+            }
+        } else if (computerChoice === 2) {
+            if (playerSelection === 3) {
+                console.log("Congragulations you beat Paper with Scissors!")
+            } else {
+                console.log("Sorry you lost to Paper")
+            }
+        } else(computerChoice === 3)
+        if (playerSelection === 1) {
+            console.log("Congragulations you beat Scissors with Rock!!")
         } else {
-            console.log("Sorry you lost Rock")
+            console.log("Sorry You lost to Scissors")
         }
-    } else if (computerChoice === 2) {
-        if (playerSelection === 3) {
-            console.log("Congragulations you beat Paper with Scissors!")
-        } else {
-            console.log("Sorry you lost to Paper")
-        }
-    } else(computerChoice === 3)
-    if (playerSelection === 1) {
-        console.log("Congragulations you beat Scissors with Rock!!")
-    } else {
-        console.log("Sorry You lost to Scissors")
     }
-}
 
 console.log()
