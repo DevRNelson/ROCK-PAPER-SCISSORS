00269096134f482fd8dd054f316abaac3bfdc3ab
Rinaldo Nelson <rinaldo.nelson1@gmail.com>
2022-09-27 14:44:14 -0400

    REWERITING JS TO WORK WITH WEBSITE

--- JS/script.js
+++ JS/script.js
@@ -1,33 +1,13 @@
+const playerScore = document.querySelector("#playerScore")
+const computerScore = document.querySelector("computerScore")
+const choiceBtns = document.querySelectorAll(".choiceBtn")
 let rock = "ROCK!"
 let paper = "PAPER!"
 let scissors = "SCISSORS"
-let playerSelection =
+let playerSelection;
+let computer;
 
+choiceBtns.forEach(button.addEventListener("click", () => {
+    playerSelection = button.textContent;
 
-
-    function playRound() {
-        let computerChoice = math.floor(math.random() * 3)
-
-        if (playerSelection === getComputerChoice) {
-            console.log("it's a tie!")
-        } else if (computerChoice === 1) {
-            if (playerSelection === 2) {
-                console.log("Congragulations you beat Rock with Paper!!")
-            } else {
-                console.log("Sorry you lost Rock")
-            }
-        } else if (computerChoice === 2) {
-            if (playerSelection === 3) {
-                console.log("Congragulations you beat Paper with Scissors!")
-            } else {
-                console.log("Sorry you lost to Paper")
-            }
-        } else(computerChoice === 3)
-        if (playerSelection === 1) {
-            console.log("Congragulations you beat Scissors with Rock!!")
-        } else {
-            console.log("Sorry You lost to Scissors")
-        }
-    }
-
-console.log()
+}));
