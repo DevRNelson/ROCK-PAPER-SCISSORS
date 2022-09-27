d8460c8e7502929c3bde74c8ce679426d52da07f
Rinaldo Nelson <rinaldo.nelson1@gmail.com>
2022-09-27 15:04:36 -0400

    making code less messy

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,8 @@
-const playerScore = document.querySelector("#playerScore")
-const computerScore = document.querySelector("#computerScore")
-const choiceBtns = document.querySelectorAll(".choiceBtn")
-let rock = "ROCK!"
-let paper = "PAPER!"
-let scissors = "SCISSORS"
+const playerScore = document.querySelector("#player");
+const computerScore = document.querySelector("#computer");
+const resu = document.querySelector("#resultText");
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+let result;
 let playerSelection;
 let computer;
 
@@ -11,10 +10,24 @@ choiceBtns.forEach(button.addEventListener("click", () => {
 
     playerSelection = button.textContent;
     computerChoice();
-    playerScore.textContent = `Player: ${playerScore}`;
-    computerScore.textContent = `Player: ${computerScore}`;
+    player.textContent = `Player: ${playerScore}`;
+    computer.textContent = `Player: ${computerScore}`;
     resultText.textContent = checkWinner();
 }));
+
+
+function checkWinner() {
+    if (player == computer) {
+        return "DRAW!";
+    } else if (computer == "ROCK") {
+        return (player == "PAPER") ? "You Win" : "You Lose!"
+    } else if (computer == "PAPER") {
+        return (player == "SCISSORS") ? "You Win" : "You Lose!"
+    } else if (computer == "SCISSORS") {
+        return (player == "ROCK") ? "You Win" : "You Lose!"
+    }
+
+}
 
 function computerChoice() {
     const randNum = math.floor(math.random() * 3) + 1;
