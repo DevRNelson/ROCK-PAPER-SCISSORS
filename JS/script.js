346f7b94a097aceec682cba5c4d7eee886df19f9
Rinaldo Nelson <rinaldo.nelson1@gmail.com>
2024-05-10 11:31:46 -0400

    Corrected mistake in game logic and rules

--- JS/script.js
+++ JS/script.js
@@ -2,14 +2,12 @@ const playerT = document.querySelector("#player");
 const computerT = document.querySelector("#computer");
 const resultT = document.querySelector("#resultText");
 const choiceBtns = document.querySelectorAll(".choice");
-const faceReaction = document.getElementById("computerFace")
-let result;
+const faceReaction = document.getElementById("computerFace");
 let player;
 let computer;
 
 choiceBtns.forEach(button => button.addEventListener("click", () => {
-
-    player = button.textContent;
+    player = button.textContent.toUpperCase();
     computerChoice();
     playerT.textContent = `YOU CHOSE  ${player}`;
     computerT.textContent = `COMPUTER CHOSE ${computer}`;
@@ -20,44 +18,39 @@ choiceBtns.forEach(button => button.addEventListener("click", () => {
 function checkWinner() {
     if (player === computer) {
         return "It's a Tie!";
-    } else if (computer === "ROCK") {
-        return (player === "PAPER") ? "You Win!" : "You Lose!"
-    } else if (computer === "PAPER") {
-        return (player === "SCISSORS") ? "You Win!" : "You Lose!"
-    } else if (computer === "SCISSORS") {
-        return (player === "ROCK") ? "You Win!" : "You Lose!"
+    } else if (player === "ROCK" && computer === "SCISSORS" || 
+               player === "PAPER" && computer === "ROCK" || 
+               player === "SCISSORS" && computer === "PAPER") {
+        return "You Win!";
+    } else {
+        return "You Lose!";
     }
 }
-
 
 function faces() {
-    if (player == computer) {
+    if (player === computer) {
         return ":|";
-    } else if (computer == "ROCK") {
-        return (player == "PAPER") ? ">:0" : ":)"
-    } else if (computer == "PAPER") {
-        return (player == "SCISSORS") ? ">:0" : ":)"
-    } else if (computer == "SCISSORS") {
-        return (player == "ROCK") ? ">:0" : ":)"
+    } else if ((player === "ROCK" && computer === "SCISSORS") || 
+               (player === "PAPER" && computer === "ROCK") || 
+               (player === "SCISSORS" && computer === "PAPER")) {
+        return ">:0";
+    } else {
+        return ":)";
     }
-
 }
 
-
-
 function computerChoice() {
-
     const randNum = Math.floor(Math.random() * 3) + 1;
-
+    
     switch (randNum) {
         case 1:
             computer = "ROCK";
             break;
         case 2:
             computer = "PAPER";
-            break
+            break;
         case 3:
             computer = "SCISSORS";
-            break
+            break;
     }
 }
